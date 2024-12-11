ed3798cec12274b631d9ebcd0a1ebcb8f6c220f6
Petko Genchev <petko.genchev.6418@gmail.com>
2024-12-11 22:16:19 +0200

    adding components to angular

---
+++ limetech_components.client/src/app/models/basket.dto.ts
@@ -0,0 +1,7 @@
+export interface BasketDTO {
+  componentId: number;
+  componentName: string;
+  quantity: number;
+  pricePerUnit: number;
+  totalPrice: number;
+}
---
+++ limetech_components.client/src/app/services/basket.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { BasketDTO } from '../models/basket.dto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BasketService {
+  private apiUrl = '/api/basket';
+
+  constructor(private http: HttpClient) { }
+
+  getBasket(): Observable<BasketDTO[]> {
+    return this.http.get<BasketDTO[]>(`${this.apiUrl}`);
+  }
+
+  addToBasket(componentId: number): Observable<void> {
+    return this.http.post<void>(`${this.apiUrl}/add`, { componentId });
+  }
+
+  removeFromBasket(componentId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/remove/${componentId}`);
+  }
+
+  clearBasket(): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/clear`);
+  }
+}

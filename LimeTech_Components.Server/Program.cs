fe9057e8d1a266a1c01e8fffe44c903bace27dd2
Petko Genchev <petko.genchev.6418@gmail.com>
2025-01-18 22:05:13 +0200

    Clearing bugs in the program.cs and updating identity at middleware

--- LimeTech_Components.Server/Program.cs
+++ LimeTech_Components.Server/Program.cs
@@ -22,10 +22,11 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<LimeTechDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddDefaultIdentity<Customer>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<LimeTechDbContext>();
+//builder.Services.AddDefaultIdentity<Customer>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<LimeTechDbContext>();
 
 builder.Services.AddIdentity<Customer, IdentityRole>(options =>
 {
+    options.SignIn.RequireConfirmedAccount = true;
     options.Password.RequireDigit = true;
     options.Password.RequireUppercase = false;
     options.Password.RequiredLength = 6;
@@ -33,21 +34,57 @@ builder.Services.AddIdentity<Customer, IdentityRole>(options =>
     .AddEntityFrameworkStores<LimeTechDbContext>()
     .AddDefaultTokenProviders();
 
+builder.Services.AddAutoMapper(typeof(LimeTechDbContext));
+
 builder.Services.AddTransient<IComponentService,ComponentService>();
 builder.Services.AddTransient<IComponentRepository,ComponentRepository>();
 builder.Services.AddTransient<ICustomerRepository,CustomerRepository>();
 builder.Services.AddTransient<ICustomerService,CustomerService>();
 
 
-builder.Services.AddAuthentication();
+//builder.Services.AddAuthentication();
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
+//using (var scope = app.Services.CreateScope())
+//{
+//    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+//    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<Customer>>();
+
+//    var adminRole = "Admin";
+//    if (!await roleManager.RoleExistsAsync(adminRole))
+//    {
+//        await roleManager.CreateAsync(new IdentityRole(adminRole));
+//    }
+
+//    var adminEmail = "admin@limetech.com";
+//    var adminPassword = "Admin123!";
+//    if (await userManager.FindByEmailAsync(adminEmail) == null)
+//    {
+//        var adminUser = new Customer
+//        {
+//            UserName = adminEmail,
+//            Email = adminEmail,
+//            FullName = "Administrator"
+//        };
+//        var result = await userManager.CreateAsync(adminUser, adminPassword);
+//        if (result.Succeeded)
+//        {
+//            await userManager.AddToRoleAsync(adminUser, adminRole);
+//        }
+//    }
+//}
+
 using (var scope = app.Services.CreateScope())
 {
-    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<Customer>>();
+    await InitializeRolesAndAdminAsync(scope.ServiceProvider);
+}
+
+async Task InitializeRolesAndAdminAsync(IServiceProvider serviceProvider)
+{
+    var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var userManager = serviceProvider.GetRequiredService<UserManager<Customer>>();
 
     var adminRole = "Admin";
     if (!await roleManager.RoleExistsAsync(adminRole))
@@ -89,6 +126,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.MapFallbackToFile("/index.html");
+app.MapFallbackToFile("index.html");
 
 app.Run();

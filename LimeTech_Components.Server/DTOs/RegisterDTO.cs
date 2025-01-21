b446eeec8bdadb5020d5fd8efcf90ae5a8f5d9d7
Petko Genchev <petko.genchev.6418@gmail.com>
2025-01-21 22:46:35 +0200

    Adding Login DTOs

---
+++ LimeTech_Components.Server/DTOs/RegisterDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static LimeTech_Components.Server.Constants.DataConstants;
+
+namespace LimeTech_Components.Server.DTOs
+{
+    public class RegisterDTO
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(UserProfile.PasswordMinLength)]
+        public string Password { get; set; }
+    }
+}
